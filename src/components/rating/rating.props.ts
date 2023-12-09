eae2a7c18d7cabe5c359f66b6882418dda539b7e
Пользователь <example@mail.com>
2023-12-09 19:39:21 +0500

    Rating component

---
+++ src/components/rating/rating.props.ts
@@ -0,0 +1,11 @@
+import { DetailedHTMLProps, HTMLAttributes, } from "react";
+
+
+
+
+export interface RatingProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+    rating:number;
+    isEditabled?:boolean ;
+   
+    setRating?: (rating:number)=> void ;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
-import { Button, Input, Tag, Text, TextArea } from "../components"
+import { Button, Input, Rating, Tag, Text, TextArea } from "../components"
 import Heading from "../components/heading/heading"
 
 
 const index = () => {
   const [isClik, setIsClick]= useState<boolean>(false)
+   const [rating, setRating]= useState<number>(3)
+  
   return (
     <div>
   <Heading tag="h1"> heading</Heading>
@@ -13,6 +15,7 @@ const index = () => {
       <Tag color="red"> red</Tag>
       <Text size="s"> text </Text>
       <Button appearance="ghost">More Details</Button>
+       <Rating rating={rating} setRating={setRating} isEditabled={true}/>
           <TextArea placeholder="messange"/>
         <Button appearance="primary">primary</Button>
         <Input placeholder="plesholder"/>
